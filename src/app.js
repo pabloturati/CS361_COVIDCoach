8722532dc80293f20d8f1e78c396ce8cf3712748
Chaudhra <47132229+Chaudhra@users.noreply.github.com>
2020-04-15 19:16:43 -0400

    added .gitignore file

--- src/app.js
+++ src/app.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const path = require('path');
 
-
-
 const app = express();
 app.use(express.static(path.join(__dirname,'../public')));
-
-
 
 const port = process.env.PORT || 3000
 
