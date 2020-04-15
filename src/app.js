d5932aa6a714e8f07a126529e97fe619662ab39b
Chaudhra <47132229+Chaudhra@users.noreply.github.com>
2020-04-15 19:14:23 -0400

    Setup express

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+
+
+const app = express();
+app.use(express.static(path.join(__dirname,'../public')));
+
+
+
+const port = process.env.PORT || 3000
+
+app.get('',(req, res)=>{
+
+    res.send('Hello COVIDCoach');
+
+});
+
+app.listen(port, ()=>{
+    console.log(`Server is up on port ${port}`);
+});
