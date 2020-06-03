8f1fb0a8b8a1469f9f9e8ba662bb39bc3071926d
Pablo Turati <40180066+pabloturati@users.noreply.github.com>
2020-06-03 19:20:04 -0400

    Feature/message replies (#9)
    * feat(news): moves news api request to backend
    
    * fix(news): correct news html closings
    
    * fix(news): removes unnecesary key

--- src/routes/news.js
+++ src/routes/news.js
@@ -1,10 +1,18 @@
+const axios = require('axios')
 const router = require('express').Router()
 
 router.get('/', (req, res) => {
   res.redirect('/index')
 })
-router.get('/index', (req, res) => {
-  res.render('news', { title: 'Covid News' })
+router.get('/index', async (req, res) => {
+  const news = await axios.get(
+    `https://newsapi.org/v2/everything?q=COVID&apiKey=${process.env.NEWS_API_KEY}`
+  )
+  if (news instanceof Error) res.send('Error fetching news')
+  res.render('news', {
+    title: 'Covid News',
+    news: news.data.articles,
+  })
 })
 
 module.exports = router
